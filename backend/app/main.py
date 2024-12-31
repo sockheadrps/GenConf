d54a7f39993783ac8d33820224d0b2fbe0f26d0d
sockheadrps <r.p.skiles@gmail.com>
2024-12-30 23:07:01 -0500

    update server quarto location and gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 import json
-from fastapi import FastAPI, StaticFiles
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from .db.db_manager import DatabaseManager
+import uvicorn
+import os
+
+# get the path to the 
 
 generators = [
             "GEN-A3", "GEN-A2", "GEN-B3", "GEN-B2", "GEN-C3", "GEN-C2",
@@ -12,8 +17,11 @@ generators = [
         ]
 
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(base_dir, "quarto", "quarto_output")
+
 app = FastAPI()
-app.mount("/dashboard", StaticFiles(directory="path_to_quarto_output"), name="dashboard")
+app.mount("/dashboard", StaticFiles(directory=static_dir), name="dashboard")
 
 
 # Configure CORS
@@ -55,3 +63,6 @@ async def record_data(data: dict):
 async def get_gen_data(month: str, generator: str):
     data = db.get_gen_data(month, generator)
     return JSONResponse(content=data)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
